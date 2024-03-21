e0294077bf1029cab6f24fa6cbe9804743d49902
RubenMartinezC <162622985+RubenMartinezC@users.noreply.github.com>
2024-03-21 16:37:47 -0600

    Update script.js

--- 4.DOM-ejercicios/Gato/script.js
+++ 4.DOM-ejercicios/Gato/script.js
@@ -1,67 +1,57 @@
-const board = document.getElementById('board');
-const result = document.getElementById('result');
-let currentPlayer = 'X';
-let moves = 0;
+const tablero = document.getElementById('tablero');
+const turno = document.getElementById('turno');
+const mensaje = document.getElementById('mensaje');
+const juego = [];
+let jugador = 'X';
+let movimientos = 0;
 let gameOver = false;
 
-const cells = [];
+function manejarClick(event) {
+    const celda = event.target;
+    const ren = parseInt(celda.dataset.ren);
+    const col = parseInt(celda.dataset.col);
 
-// inicializa el tablero
-for (let i = 0; i < 3; i++) {
-    const row = document.createElement('div');
-    row.classList.add('clearfix');
-    for (let j = 0; j < 3; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.dataset.row = i;
-        cell.dataset.col = j;
-        cell.addEventListener('click', handleClick);
-        row.appendChild(cell);
-        cells.push(cell);
-    }
-    board.appendChild(row);
-}
+    if (gameOver || celda.textContent.length > 0) return;
 
-function handleClick() {
-    if (gameOver) return;
+    juego[ren * 3 + col] = jugador;
+    celda.textContent = jugador;
+    movimientos++;
+    validaGanador();
+    cambiaJugador();
 
-    if (!this.textContent) {
-        this.textContent = currentPlayer;
-        moves++;
-        checkWinner(parseInt(this.dataset.row), parseInt(this.dataset.col));
-        togglePlayer();
-    }
-
-    if (moves === 9 && !gameOver) {
-        result.textContent = "empate!";
+    if (movimientos === 9 && !gameOver) {
+        mensaje.textContent = 'empate!!';
         gameOver = true;
     }
+
+    console.log(juego);
 }
 
 // revisamos si es ganador comparando contra las combinaciones ganadoras
-function checkWinner(row, col) {
+function validaGanador(ren, col) {
     if (
-        checkLine(0, 0, 0, 1, 0, 2) || // linea 0
-        checkLine(1, 0, 1, 1, 1, 2) || // linea 1
-        checkLine(2, 0, 2, 1, 2, 2) || // linea 2
-        checkLine(0, 0, 1, 0, 2, 0) || // columna 0
-        checkLine(0, 1, 1, 1, 2, 1) || // columna 1
-        checkLine(0, 2, 1, 2, 2, 2) || // columna 2
-        checkLine(0, 0, 1, 1, 2, 2) || // diagonal \
-        checkLine(0, 2, 1, 1, 2, 0) // diagonal /
+        linea(0, 0, 0, 1, 0, 2) || // linea 0
+        linea(1, 0, 1, 1, 1, 2) || // linea 1
+        linea(2, 0, 2, 1, 2, 2) || // linea 2
+        linea(0, 0, 1, 0, 2, 0) || // columna 0
+        linea(0, 1, 1, 1, 2, 1) || // columna 1
+        linea(0, 2, 1, 2, 2, 2) || // columna 2
+        linea(0, 0, 1, 1, 2, 2) || // diagonal \
+        linea(0, 2, 1, 1, 2, 0) // diagonal /
     ) {
-        result.textContent = `jugador ${currentPlayer} gana!`;
+        mensaje.textContent = `jugador ${jugador} gana!`;
         gameOver = true;
     }
 }
 
 // reviso si el jugador tiene una combinacion ganadora
-function checkLine(r1, c1, r2, c2, r3, c3) {
-    return cells[r1 * 3 + c1].textContent === currentPlayer &&
-        cells[r2 * 3 + c2].textContent === currentPlayer &&
-        cells[r3 * 3 + c3].textContent === currentPlayer;
+function linea(r1, c1, r2, c2, r3, c3) {
+    return juego[r1 * 3 + c1] === jugador &&
+        juego[r2 * 3 + c2] === jugador &&
+        juego[r3 * 3 + c3] === jugador;
 }
 
-function togglePlayer() {
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+function cambiaJugador() {
+    jugador = jugador === 'X' ? 'O' : 'X';
+    turno.textContent = `Turno de ${jugador}`;
 }
